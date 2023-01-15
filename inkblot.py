b4a0cc101e215482fdf90b1b81bd26ec6a450e1d
Anchith Acharya U <anchithacharya@gmail.com>
2023-01-16 00:47:15 +0530

    performance

--- inkblot.py
+++ inkblot.py
@@ -1,21 +1,20 @@
 import tkinter as tk
 import numpy as np
-from opensimplex import noise3array, random_seed, seed as os_seed
+from opensimplex import noise3array, random_seed
 from PIL import Image, ImageTk
 
 
 IMG_DIM_X = 1000
 IMG_DIM_Y = 500
+SHIFT_SPEED = 0.05
 
 
+random_seed()
 x_y_axis = np.arange(0,IMG_DIM_X/2)/100
-def gen_noise(z_axis = None, seed = None):
+def gen_noise(z_axis = None):
     if z_axis is None: z_axis = x_y_axis*10
 
-    if seed: os_seed(seed)
-    else: random_seed()
-
-    return noise3array(x_y_axis, x_y_axis, z_axis)
+    return noise3array(x_y_axis, x_y_axis, z_axis)[0]
 
 depth_mask = np.zeros(shape=(IMG_DIM_X,IMG_DIM_Y))
 for x in range(IMG_DIM_X):
@@ -41,16 +40,6 @@ def gen_img(noise):
     return img
 
 
-# noise = gen_noise()
-# imgs = []
-
-# for frame in range(10):
-#   img = gen_img(noise[frame])
-#   imgs.append(img)
-
-# imgs[0].save('rorschach.webp', save_all=True, append_images=imgs[1:], )
-
-
 root = tk.Tk()
 root.geometry(f"{IMG_DIM_X}x{IMG_DIM_Y}")
 root.resizable(False, False)
@@ -61,13 +50,13 @@ imgHeight = (root.winfo_width(), root.winfo_height())
 label = tk.Label(root)
 label.pack(expand=True, fill="both")
 
-z_axis = np.zeros(500)
+z_axis = np.zeros(1)
 def update_img():
     global z_axis
-    z_axis += 0.01
+    z_axis += SHIFT_SPEED
     noise = gen_noise(z_axis)
     
-    image = gen_img(noise[0])
+    image = gen_img(noise)
     imgTK = ImageTk.PhotoImage(image.resize(imgHeight, resample=Image.NEAREST))
     label.configure(image=imgTK)
     label.update()
