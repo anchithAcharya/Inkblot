d6bb9aa6392da85bba9b6d360f3bb147201b5d94
ANCHITU <anchith_acharya.u@daimler.com>
2023-01-19 14:18:28 +0530

    added more controls

--- inkblot.py
+++ inkblot.py
@@ -7,10 +7,13 @@ from PIL import Image, ImageTk
 IMG_DIM_X = 1000
 IMG_DIM_Y = 500
 SHIFT_SPEED = 0.05
+THRESHOLD_MASK = True
+THRESHOLD_VALUE = 128
+PAUSE = False
 
 
 random_seed()
-x_y_axis = np.arange(0,IMG_DIM_X/2)/100
+x_y_axis = np.arange(0,IMG_DIM_X/2)/75
 def gen_noise(z_axis = None):
     if z_axis is None: z_axis = x_y_axis*10
 
@@ -24,6 +27,19 @@ for x in range(IMG_DIM_X):
 
         depth_mask[x][y] = (1-nx**2) * (1-ny**2)
 
+TOP = 0
+BOTTOM = 1
+PULL = 50
+
+def contrast(num):
+	delta = num * (PULL/100)
+
+	if num > (TOP + BOTTOM)/2:
+		return delta
+	
+	else:
+		return BOTTOM - delta
+
 def gen_img(noise):
     img = Image.new('RGB', (IMG_DIM_X, IMG_DIM_Y), 'white')
 
@@ -31,14 +47,31 @@ def gen_img(noise):
         for j, value in enumerate(row):
             d = depth_mask[i][j]
             value = (value+1)/2
+            value = contrast(value)
             value = int(value * 256 * d)
-            if value > 128:
-                value = 0
+
+
+            threshold = THRESHOLD_VALUE if THRESHOLD_MASK else 0
+            if value > threshold:
+                if THRESHOLD_MASK: value = 0
                 img.putpixel((i,j), (value, value, value))
                 img.putpixel((-i-1,j), (value, value, value))
 
     return img
 
+def click_func(_):
+    global THRESHOLD_MASK
+    THRESHOLD_MASK = not THRESHOLD_MASK
+
+def pause(_):
+    global PAUSE
+    PAUSE = not PAUSE
+
+def change_threshold(e):
+    global THRESHOLD_VALUE, PULL
+
+    if THRESHOLD_MASK: THRESHOLD_VALUE -= (e.delta / 120)
+    else: PULL -= (e.delta / 120)
 
 root = tk.Tk()
 root.geometry(f"{IMG_DIM_X}x{IMG_DIM_Y}")
@@ -49,11 +82,15 @@ imgHeight = (root.winfo_width(), root.winfo_height())
 
 label = tk.Label(root)
 label.pack(expand=True, fill="both")
+label.bind("<Button-1>", click_func)
+label.focus_set()
+label.bind("<space>", pause)
+label.bind("<MouseWheel>", lambda e: change_threshold(e))
 
 z_axis = np.zeros(1)
 def update_img():
     global z_axis
-    z_axis += SHIFT_SPEED
+    if not PAUSE: z_axis += SHIFT_SPEED
     noise = gen_noise(z_axis)
     
     image = gen_img(noise)
